bc0d37cf1608b8549db6ee556887c3af078f9bd8
Juanes Saavedra <juanessaavedra777@gmail.com>
2024-09-25 14:58:46 -0500

    Update alfabeticamenteRecursividad

--- practica_recursividad/src/AlfabeticamenteRecursividad.java
+++ practica_recursividad/src/AlfabeticamenteRecursividad.java
@@ -1,19 +1,29 @@
 public class AlfabeticamenteRecursividad {
     public static void main(String[] args) {
 
-        int n=0;
-        int stringLength = string.length();
+        String word = "ABCDFE";
 
+        boolean resultado = alfabeticamenteRecursivo(word);
 
-        String name = "ab";
+        System.out.println(resultado);
 
+    }
 
-        if ((name.charAt(0) < name.charAt(1+1))) {
-            int n = n+1
+    public static boolean alfabeticamenteRecursivo(String word) {
+        String word;
+
+        if ((name.charAt(0) < name.charAt(1 + 1))) {
+            int n = n + 1
 
         } else {
             System.out.println("No esta organizado");
         }
+        return true;
     }
-}
+        }
+
+
+
+
+
 
